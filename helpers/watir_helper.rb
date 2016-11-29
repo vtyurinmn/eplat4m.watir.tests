f2138caabcb5e1454ede86cdab39f094a5b0433b
Grim Panda <beloved.kadavr@gmail.com>
2016-11-29 14:36:31 +0500

    finalising first lab…

--- helpers/watir_helper.rb
+++ helpers/watir_helper.rb
@@ -20,17 +20,23 @@ module Helpers
   end
 
   # сохраняемся и машем ручкой (при работе с источниками данных решения)
-  def app_save_and_exit
-    @browser.link(href: /dataSources/).when_present.click
+  def app_save_and_exit(whereto)
+    @browser.link(href: /#{whereto}/).when_present.click
     @browser.button(value: 'Сохранить и перейти').when_present.click
+    sleep(5)
   end
 
   # добавление ролей (используем меню Безопаность)
   def add_role(role_name)
-    @browser.button(value: 'Добавить').when_present.click
+    sleep(5)
+    @browser.button(xpath: 'html/body/div[1]/div/div/div[4]/div/div/div/button').when_present.click #кнопка "Добавить"
     @browser.text_field(id: 'roleName').set(role_name)
+    @browser.execute_script("$('#roleName').trigger('change')")
     @browser.button(value: 'Создать').click
-    @browser.button(value: 'Сохранить').click
+    sleep(5)
+    @browser.button(xpath: 'html/body/div[1]/div/div/div[4]/div/div/div/button').when_present.click #кнопка "Сохранить"
+    @browser.link(href:'#security/roles/').when_present.click
+    #вернуться на вкладку "Роли"
   end
 
   # делаем клевые фоточки
@@ -38,9 +44,26 @@ module Helpers
     @browser.screenshot.save ("../shots/#{filename}.png")
   end
 
-  ## МЕТОДЫ ДЛЯ РАБОТЫ С РЕШЕНИЯМИ
-  # создание нового решения
+  ##ЗАЛЕПУХИ
 
+  #ожидание пока не появится элемент
+  def wait(element)
+    until element.exists? && element.visible?
+      sleep(1)
+    end
+  end
+
+  #ожидаем пока кнопка не станет кликабельной
+  def button_enabled(button_name)
+    until @browser.execute_script("return $._data($('.btn.btn-panel:contains(\"#{button_name}\")').get(0),
+'events').click != null")
+      sleep(1)
+      end
+  end
+
+  ## МЕТОДЫ ДЛЯ РАБОТЫ С РЕШЕНИЯМИ
+
+  # создание нового решения
   def create_solution(solution_name, solution_description)
     @browser.link(text: 'Администрирование').when_present.click
     @browser.link(text: 'Решения').click
@@ -59,6 +82,7 @@ module Helpers
     @browser.button(title: 'Создать приложение').when_present.click
     @browser.text_field(id: 'applicationName').when_present.set(app_name)
     @browser.button(value: 'Создать').click
+    sleep(5)
     @browser.link(href: /fields/).when_present.click
   end
 
@@ -92,6 +116,16 @@ module Helpers
     @browser.button(value: 'Ok').click
   end
 
+  # добавление нового поля типа "Состояние"
+  def create_state_field(field_name, field_index)
+    @browser.div(css: '.panel.panel-default.text-center', index: 3).when_present.click
+    @browser.text_field(class: 'columnNameInput', index: field_index).set(field_name)
+    @browser.link(class: 'link-button-grid', title: 'Настройки', index: field_index).click
+    @browser.link(css: '.select2-choice.select2-default').when_present.click
+    @browser.div(class: 'select2-result-label').when_present.click
+    @browser.button(value: 'Ok').click
+  end
+
   # добавление свойства "Имя объекта" для поля приложения типа "Текст"
   def set_name_for_link(field_index)
     @browser.link(class: 'link-button-grid', title: 'Настройки', index: field_index).click
@@ -110,7 +144,9 @@ module Helpers
 
   # создание нового провайдера
   def add_provider(provider_name)
-    @browser.button(css: '.btn.btn-panel.btn-xs', index: 2).click
+    wait(@browser.button(css: '.btn.btn-panel.btn-xs:nth-of-type(3)'))
+    button_enabled('Создать провайдер данных')
+    @browser.button(css: '.btn.btn-panel.btn-xs:nth-of-type(3)').click
     @browser.text_field(id: 'name').when_present.set(provider_name)
     @browser.button(value: 'Создать').click
   end
@@ -128,9 +164,19 @@ module Helpers
     @browser.link(css: '.btn.btn-primary', index: bond_index).when_present.click
   end
 
-  # переключить вкладку
+  # переключить вкладку (c href)
+  def inset_change_href(app_inset)
+    @browser.link(href: app_inset).when_present.click
+  end
+
+  # переключить вкладку (по text)
   def inset_change(app_inset)
     @browser.link(text: app_inset).when_present.click
+  end
+
+  # переключить вкладку (по xpath)
+  def inset_change_xpath(app_inset)
+    @browser.link(xpath: app_inset).when_present.click
   end
 
   #	выбор полей из приложений
@@ -147,16 +193,16 @@ module Helpers
   end
 
   # добавление группы состояний
-  def state_grout_set(state_name, state_desc)
-    @browser.button(value: 'Добавить').when_present.click
+  def state_grout_set(state_name)
+    @browser.button(css: '.btn.btn-panel.btn-xs').when_present.click
     @browser.text_field(id: 'stateGroupName').when_present.set(state_name)
-    @browser.area(id: 'stateGroupDescription').set(state_desc)
     @browser.button(value: 'Создать').click
   end
 
   # добавление начального состояния
   def beg_state(state_name)
-    @browser.button(value: 'Добавить').click
+    sleep(5)
+    @browser.button(title: 'Добавить').click
     @browser.text_field(id: 'stateName').when_present.set(state_name)
     @browser.checkbox(id: 'stateStart').set
     @browser.button(value: 'Создать').click
@@ -164,18 +210,39 @@ module Helpers
 
   # добавление состояния
   def new_state(state_name)
-    @browser.button(value: 'Добавить').click
+    sleep(5)
+    @browser.button(title: 'Добавить').click
     @browser.text_field(id: 'stateName').when_present.set(state_name)
     @browser.button(value: 'Создать').click
   end
   
   #добавление перехода между состояниями
   def state_trans(trans_index, trans_name)
-    @browser.link(title: 'Перейти', index: trans_index).click
-    @browser.link(text: 'Редактор переходов').when_present.click
-    @browser.button(value: 'Добавить').when_present.click
+    sleep(5)
+    @browser.link(css: '.dx-link.link-button-grid', index: trans_index).click
+    # переключаемся на "Редактор переходов"...
+    @browser.link(xpath: 'html/body/div[1]/div/div/div[4]/div/section/div/div/div/div/ul/li[2]/a').when_present.click
+    # ...и жмем кнопку "Добавить"
+    @browser.button(xpath: 'html/body/div[1]/div/div/div[4]/div/div/div/button').when_present.click
     @browser.select_list(id: 'transition').when_present.select(trans_name)
     @browser.button(value: 'Добавить').click
   end
-  
+
+  def role_trans(role_name)
+    sleep(5)
+    # переключаемся на "Редактор переходов"...
+    @browser.link(xpath: 'html/body/div[1]/div/div/div[4]/div/section/div/div/div/div/ul/li[3]/a').when_present.click
+    # ...и жмем кнопку "Добавить"
+    @browser.button(xpath: 'html/body/div[1]/div/div/div[4]/div/div/div/button[2]').when_present.click
+    @browser.select_list(id: 'role').when_present.select(role_name)
+    @browser.button(value: 'Добавить').click
+  end
+
+  def state_app(app_index, field_name, field_index)
+    sleep(5)
+    @browser.link(class: 'dx-link link-button-grid', title: 'Перейти', index: app_index).when_present.click
+    @browser.link(href: /fields/).when_present.click
+    create_state_field(field_name, field_index)
+    @browser.button(css:'button.btn-panel:nth-child(1)').when_present.click
+  end
 end
--- rspec/first_lab_spec.rb
+++ rspec/first_lab_spec.rb
@@ -16,6 +16,14 @@ describe 'EPLAT' do
     make_screenshot('выполнен вход')
   end
 
+  it 'созданы роли' do
+    inset_change('Безопасность')
+    inset_change('Роли')
+    add_role('Инженер СТП')
+    add_role('Менеджер качества СТП')
+    make_screenshot('созданы роли')
+  end
+
   it 'создано новое решение' do
     create_solution('Обработка заявок','тестовое решение')
     make_screenshot('создано новое решение')
@@ -30,7 +38,7 @@ describe 'EPLAT' do
     create_text_field('E-mail', 4)
     set_name_for_link(0)
     make_screenshot('добавлено приложение Клиент')
-    app_save_and_exit
+    app_save_and_exit('dataSources')
   end
 
   it 'добавлено приложение Представители' do
@@ -41,7 +49,7 @@ describe 'EPLAT' do
     create_text_field('Должность', 2)
     create_text_field('Контактный телефон', 3)
     make_screenshot('добавлено приложение Представители')
-    app_save_and_exit
+    app_save_and_exit('dataSources')
   end
 
   it 'добавлено приложение Проекты' do
@@ -55,7 +63,7 @@ describe 'EPLAT' do
     create_connection_field('Представитель', 5, 'Представители', 'Многие к одному')
     create_connection_field('Менеджер', 6, 'Пользователи', 'Многие к одному')
     make_screenshot('добавлено приложение Проекты')
-    app_save_and_exit
+    app_save_and_exit('dataSources')
   end
 
   it 'добавлено приложение Заявки' do
@@ -73,100 +81,94 @@ describe 'EPLAT' do
     create_text_field('Работы по заявке', 8)
     create_date_field('Дата завершения работы', 9)
     make_screenshot('добавлено приложение Заявки')
-    app_save_and_exit
+    app_save_and_exit('dataSources')
   end
 
   it 'добавлен провайдер Заявки по времени' do
-    add_provider('Заявки по проектам')
+    add_provider('Заявки по времени')
     add_app('Заявки')
-    inset_change('Поля')
+    inset_change_href('#field')
     choose_field(0, 'Идентификатор')
     select_func(0, 'Количество')
     choose_field(1, 'Дата регистрации')
     select_func(1, 'Группировка')
     make_screenshot('добавлен провайдер Заявки по времени')
-    app_save_and_exit
+    app_save_and_exit('dataSources')
   end
 
   it 'добавлен провайдер Заявки по клиентам' do
     add_provider('Заявки по клиентам')
     add_app('Заявки')
     bond_app(2)
-    inset_change('Поля')
+    inset_change_href('#field')
     choose_field(0, 'Идентификатор')
     select_func(0, 'Количество')
     choose_field(1, 'Название')
     select_func(1, 'Группировка')
     make_screenshot('добавлен провайдер Заявки по клиентам')
-    app_save_and_exit
+    app_save_and_exit('dataSources')
   end
 
   it 'добавлен провайдер Заявки по проектам' do
-    add_provider('Заявки по клиентам')
+    add_provider('Заявки по проектам')
     add_app('Заявки')
     bond_app(2)
-    inset_change('Поля')
+    inset_change_href('#field')
     choose_field(0, 'Идентификатор')
     select_func(0, 'Количество')
     choose_field(1, 'Номер')
     select_func(1, 'Группировка')
-    make_screenshot('добавлен провайдер Заявки по клиентам')
-    app_save_and_exit
+    make_screenshot('добавлен провайдер Заявки по проектам')
+    app_save_and_exit('dataSources')
   end
 
   it 'добавлена группа состояний' do
     inset_change('Администрирование')
     inset_change('Состояния')
-    state_grout_set('Состояния заявки', 'Статусы заявок')
+    state_grout_set('Состояния заявки')
     make_screenshot('добавлена группа состояний')
-    app_save_and_exit
   end
 
   it 'добавлено состояние Новая' do
-    inset_change('Состояния')
+    inset_change_xpath('html/body/div[1]/div/div/div[4]/div/section/div/div/ul/li[2]/a')
     beg_state('Новая')
     make_screenshot('добавлено состояние Новая')
-    app_save_and_exit
   end
 
   it 'добавлено состояние В работе' do
-    inset_change('Состояния')
     new_state('В работе')
     make_screenshot('добавлено состояние В работе')
-    app_save_and_exit
   end
 
+
   it 'добавлено состояние Выполнено' do
-    inset_change('Состояния')
     new_state('Выполнено')
     make_screenshot('добавлено состояние Выполнено')
-    app_save_and_exit
   end
 
   it 'добавлено состояние Закрыта' do
-    inset_change('Состояния')
     new_state('Закрыта')
     make_screenshot('добавлено состояние Закрыта')
-    app_save_and_exit
   end
 
   it 'настроены переходы между состояниями' do
     state_trans(0, 'В работе')
-    inset_change('Состояния')
+    role_trans('Инженер СТП')
+    inset_change_xpath('html/body/div[1]/div/div/div[4]/div/section/div/div/ul/li[2]/a') #переход в "Состояния"
     state_trans(1, 'Выполнено')
-    inset_change('Состояния')
+    role_trans('Менеджер качества СТП')
+    inset_change_xpath('html/body/div[1]/div/div/div[4]/div/section/div/div/ul/li[2]/a')
     state_trans(2, 'В работе')
-    inset_change('Состояния')
-    state_trans(2, 'Закрыта') # =(
+    role_trans('Менеджер качества СТП')
+    inset_change_xpath('html/body/div[1]/div/div/div[4]/div/section/div/div/ul/li[2]/a')
+    state_trans(2, 'Закрыта')
     make_screenshot('настроены переходы между состояниями')
   end
 
-  it 'созданы роли' do
-    inset_change('Безопасность')
-    inset_change('Роли')
-    add_role('Инженер СТП')
-    add_role('Менеджер качества СТП')
-    make_screenshot('созданы роли')
+  it 'добавлена Связь группы состояний с приложением' do
+    inset_change('Администрирование')
+    inset_change('Приложения')
+    state_app(3, 'Состояние', 8)
+    make_screenshot('добавлена Связь группы состояний с приложением')
   end
-  
 end
